33f4fd4430b816f0465966f98660b60ed44501f6
emelineB <emlinebruyere@gmail.com>
2022-12-27 19:33:19 +0100

    Ajout de commentaires sur rôle des fonctions

--- simulation.h
+++ simulation.h
@@ -31,13 +31,26 @@ typedef struct Coccinelle {
     int direction;
 } Coccinelle;
 
-int pousseTomate();
-void mortTomate();
-void deplacementPuceron() ; //direction donn´ee jusqu’`a ce qu’ils ne trouvent plus de tomates, auquel cas ils se dirigeront dans une case attenante au hasard contenant une tomate. Si ils n’en trouvent pas, ils se d´eplaceront sur une case au hasard.
-void deplacementCoccinelle();
-void vieillissementCoccinelle();
-void vieillissementPuceron();
-void reproductionPuceron();
-void reproductionCoccinelle();
-void mortPuceron();
-void mortCoccinelle();
+
+//Fonctions relatives aux tomates
+void pousseTomate(Tomate* tomate);
+/*Prend en entrée un pointeur sur un objet de type Tomate et met à jour l'état de croissance de cette tomate. Si l'état est compris entre 0 et 4, alors ajoute +1 à celui-ci. Sinon ne fait rien.*/
+
+void mortTomate(Tomate* tomate);
+/*Prend en entrée un pointeur sur un objet de type Tomate et met son état de croissance à 0.*/
+
+
+//Fonctions relatives aux pucerons
+void deplacementPuceron(Puceron* puceron) ;
+/*Direction donnée jusqu’à ce qu’ils ne trouvent plus de tomates, auquel cas ils se dirigeront dans une case attenante au hasard contenant une tomate. Si ils n’en trouvent pas, ils se d´eplaceront sur une case au hasard.*/
+
+void vieillissementPuceron(Puceron* puceron);
+void reproductionPuceron(Puceron* puceron);
+void mortPuceron(Puceron* puceron);
+
+
+//Fonctions relatives aux coccinelles
+void deplacementCoccinelle(Coccinelle* coccinelle);
+void vieillissementCoccinelle(Coccinelle* coccinelle);
+void reproductionCoccinelle(Coccinelle* coccinelle);
+void mortCoccinelle(Coccinelle* coccinelle);
