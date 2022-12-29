c2b7df6a1ea6f3ea98ac4501c634ab72cc8bf928
emelineB <emlinebruyere@gmail.com>
2022-12-29 19:22:54 +0100

    Add : implémentation des fonctions Tomates

--- simulation.h
+++ simulation.h
@@ -1,16 +1,12 @@
 #ifndef simulation
 #define simulation
 
-typedef struct Coordonnee {
-    int abscisse;
-    int ordonnee;
-} Coordonnee;
+#include<stdio.h>
 
-typedef struct Case {
-    Tomate* tomateCase;
-    Puceron* puceronCase;
-    Coccinelle* coccinelleCase;
-} Case;
+typedef struct Coordonnees {
+    int x;
+    int y;
+} Coordonnees;
 
 typedef struct Tomate {
     Coordonnees coordTomates;
@@ -31,10 +27,16 @@ typedef struct Coccinelle {
     int direction;
 } Coccinelle;
 
+typedef struct Case {
+    Tomate* tomateCase;
+    Puceron* puceronCase;
+    Coccinelle* coccinelleCase;
+} Case;
+
 
 //Fonctions relatives aux tomates
 void pousseTomate(Tomate* tomate);
-/*Prend en entrée un pointeur sur un objet de type Tomate et met à jour l'état de croissance de cette tomate. Si l'état est compris entre 0 et 4, alors ajoute +1 à celui-ci. Sinon ne fait rien.*/
+/*Prend en entrée un pointeur sur un objet de type Tomate et met à jour l'état de croissance de cette tomate. Si l'état est compris entre 0 et 19, alors ajoute +1 à celui-ci. Sinon ne fait rien.*/
 
 void mortTomate(Tomate* tomate);
 /*Prend en entrée un pointeur sur un objet de type Tomate et met son état de croissance à 0.*/
@@ -54,3 +56,5 @@ void deplacementCoccinelle(Coccinelle* coccinelle);
 void vieillissementCoccinelle(Coccinelle* coccinelle);
 void reproductionCoccinelle(Coccinelle* coccinelle);
 void mortCoccinelle(Coccinelle* coccinelle);
+
+# endif
